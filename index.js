c692e5375ad6455982705e1d4fa5d9a796de4b4f
unknown <anik.fahim07@gmail.com>
2023-10-08 14:03:38 +0600

    mongodb version

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.x4eccmn.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,26 +24,49 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const treeCollection = client.db("leafy-Universe").collection("treeCollection")
+    // __________Database collection__________
+    const treeCollection = client
+      .db("leafy-Universe")
+      .collection("treeCollection");
+    const orderCollection = client.db("orderCollection").collection("orders");
 
-    app.get('/trees', async (req, res)=>{
-        const result = await treeCollection.find().toArray()
-        res.send(result)
-        
-    })
-    app.get('/trees/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) }
+    // __________Trees api_________
+    app.get("/trees", async (req, res) => {
+      const result = await treeCollection.find().toArray();
+      res.send(result);
+    });
+    // find single tree api get
+    app.get("/trees/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
 
-        const result = await treeCollection.findOne(query);
-        res.send(result);
-    })
+      const result = await treeCollection.findOne(query);
+      res.send(result);
+    });
+    // add tree api post
+    app.post("/trees", async (req, res) => {
+      const tree = req.body;
+      // console.log(tree)
+      const result = await treeCollection.insertOne(tree);
+      console.log(result);
+      res.send(result);
+    });
 
-
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+    app.get("/orders", async (req, res) => {
+      const order = await orderCollection.find().toArray();
+      res.send(order);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -56,11 +74,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("leafy universe");
+});
 
-app.get('/',(req,res)=>{
-    res.send("leafy universe")
-})
-
-app.listen(port, ()=>{
-    console.log(`listening on port${port}`)
-})
+app.listen(port, () => {
+  console.log(`listening on port${port}`);
+});
